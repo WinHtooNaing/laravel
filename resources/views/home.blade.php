646115c641d2f72122588141c53743e0281a96d7
Win Htoo Naing <winhtoonaing2003@gmail.com>
2023-10-22 10:34:07 +0630

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+<div class="card">
+  <div class="card-header" style="text-align: center;">
+    Contents
+  </div>
+  @foreach($data as $post)
+  <div class="card-body">
+    <h5 class="card-title">{{$post->name}}</h5>
+    <p class="card-text">{{$post->description}}</p>
+    <a href="#" class="btn btn-primary">view</a>
+  </div><hr>
+  @endforeach
+</div>
+</div>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<html>
+     <head>
+     <title>WHN laravel</title>
+
+    </head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    
+    <body>
+    <h2 style="text-align: center;">Sample Blog Site</h2>  <br>  
+    
+        @yield('content')
+    </body>
+</html>
